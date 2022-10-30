562a126d4e454874e8866579bcad9a67ae123cd3
Виктор Чернюк <vetrolego@gmail.com>
2022-10-30 13:41:09 +0300

    deleted extra strings

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
  import React from 'react';
  import type {Node} from 'react';
  import {
@@ -28,41 +20,7 @@
    ReloadInstructions,
  } from 'react-native/Libraries/NewAppScreen';
  
- /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
-  * LTI update could not be added via codemod */
- const Section = ({children, title}): Node => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
- 
- const App: () => Node = () => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
- 
+ const App = () => {
    return (
      <View style={styles.mainContainer}>
        <ImageBackground source={require('./img/bgImg.jpg')} resizeMode="cover" style={styles.bgImg}>
